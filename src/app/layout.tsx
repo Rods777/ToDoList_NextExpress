3dd9e3e52c24a20f698a3f65f54c209c016bd676
Rods777 <rodelcuyag123@gmail.com>
2025-08-31 12:24:33 +0800

    font changed to Poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap', // Ensures text is visible during font loading
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], // Specify desired weights
+  variable: '--font-poppins',
 });
 
 export const metadata: Metadata = {
@@ -27,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.variable} antialiased`}
       >
         <Theme accentColor="crimson" grayColor="sand" radius="large" scaling="95%">{children}</Theme>
       </body>
